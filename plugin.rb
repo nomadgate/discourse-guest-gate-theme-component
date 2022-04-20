b3f29fb2eefca326859e2cc5dfb1e58d95031f8e
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-04-20 15:35:59 +0200

    Delete plugin.rb

--- plugin.rb
+++
@@ -1,9 +0,0 @@
-# name: guest-gate
-# about: Force guest users to create an account by preventing them from seeing more topics
-# version: 0.3
-# authors: Vinoth Kannan (vinothkannan@vinkas.com) and jgujgu and michael@discoursehosting.com
-# url: https://github.com/discoursehosting/discourse-guest-gate
-
-enabled_site_setting :guest_gate_enabled
-
-register_asset "stylesheets/guest-gate.scss"
